868cf83ddf0b8bb170c6c9022d2248b22d299528
Do Nhat Phong <nhatphongdo@gmail.com>
2013-03-07 14:15:43 +0700

    Add tracking code

--- src/ForBoss2/src/com/forboss/MainActivity.java
+++ src/ForBoss2/src/com/forboss/MainActivity.java
@@ -11,6 +11,8 @@ import com.forboss.data.api.APIHelper;
 import com.forboss.data.model.CommonData;
 import com.forboss.util.ForBossUtils;
 
+import com.tradedoubler.sdk.MobileSDK;
+
 public class MainActivity extends Activity {
 
 	private MainActivity getContext() {
@@ -20,6 +22,23 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// Start of App Download Tracking
+		// Values for the trackback... you should have received them from Tradedoubler
+		String orgId = "1791723";
+		String event = "276896";
+		String secretCode = "699544286"; // Validate the trackback with this in our side String currency = "EUR";
+		String orderValue = "1.49";
+		boolean testMode = false; // Never set this to true in an app that goes in to production int timeout = 5; // Timeout in seconds
+		int lifeTimeValue = 10; // For how many days should we track
+		boolean trackAsSale = true; // Only used in this example
+		MobileSDK mobileSDK = MobileSDK.getInstance(this);
+		if(trackAsSale) { mobileSDK.trackDownloadAsSale(
+		orgId, event, secretCode, "USD", orderValue, testMode, 5, lifeTimeValue );
+		} else { mobileSDK.trackDownloadAsLead(
+		orgId, event, secretCode, testMode, 5, lifeTimeValue );
+		}
+		// End of App Download Tracking
 		
 		Boolean shouldCloseApp = (Boolean) ForBossUtils.getBundleData("shouldCloseApp");
 		if (shouldCloseApp != null && shouldCloseApp.booleanValue()) {
